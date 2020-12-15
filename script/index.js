d73fcc220d521a92b9797f3903f28f0458972ea1
hidemusiam <hidemusiam@gmail.com>
2020-12-16 08:40:58 +0900

    Gメール作成フォーム作成完了

---
+++ script/index.js
@@ -0,0 +1,64 @@
+/**
+ * 初期表示処理
+ */
+function initialize() {
+  document.getElementById("address_to").value = '';
+  document.getElementById("address_cc").value = '';
+  document.getElementById("address_bcc").value = '';
+}
+
+/**
+ * Gメールの送信画面を起動する
+ */
+function createMail() {
+
+  // URLを作成する
+  var url = getURL();
+
+  // 新しいタブで開く
+  window.open(url);
+}
+
+/**
+ * Gメール送信用URLを作成する
+ * @return  url
+ */
+function getURL() {
+  var url = "https://mail.google.com/mail/?view=cm&fs=1";
+  // 宛先を含める
+  var inputvalue = document.getElementById("address_to").value;
+  if (inputvalue != "") {
+    url += "&to=";
+    url += inputvalue;
+  }
+  // CCを含める
+  inputvalue = document.getElementById("address_cc").value;
+  if (inputvalue != "") {
+    url += "&cc=";
+    url += inputvalue;
+  }
+  // BCCを含める
+  inputvalue = document.getElementById("address_bcc").value;
+  if (inputvalue != "") {
+    url += "&bcc=";
+    url += inputvalue;
+  }
+  // タイトルを含める
+  inputvalue = document.getElementById("title").value;
+  if (inputvalue != "") {
+    url += "&su=";
+    url += inputvalue;
+  }
+  // 本文を含める
+  inputvalue = document.getElementById("mailbody").value;
+  if (inputvalue != "") {
+    url += "&body=";
+    url += inputvalue;
+  }
+  return url;
+}
+
+// loadイベント
+window.onload = function() {
+  initialize();
+};
