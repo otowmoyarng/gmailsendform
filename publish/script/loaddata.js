ccd3c63438255e7e612f31eed1ba1d19c793a168
otowmoyarng <hidemusiam@gmail.com>
2022-02-01 22:57:18 +0900

    ディレクトリ変更

---
+++ publish/script/loaddata.js
@@ -0,0 +1,13 @@
+// 初期表示値
+const LoadData = {
+    // 宛先
+    AddressTo: 'to@gmail.com',
+    // ＣＣ
+    AddressCc: 'cc@gmail.com',
+    // ＢＣＣ
+    AddressBcc: 'bcc1@gmail.com, bcc1@gmail.com',
+    // 件名
+    Title: 'title',
+    // 本文
+    MailBody: 'body'
+}
